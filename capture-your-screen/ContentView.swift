48a00f433162273918b624a7af81a5c5694ac6d6
mammut001 <dongpei110@gmail.com>
2026-04-03 12:12:46 -0400

    Initial Commit

---
+++ capture-your-screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capture-your-screen
+//
+//  Created by Yuhan Song on 2026-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ capture-your-screen/capture_your_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  capture_your_screenApp.swift
+//  capture-your-screen
+//
+//  Created by Yuhan Song on 2026-04-03.
+//
+
+import SwiftUI
+
+@main
+struct capture_your_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
